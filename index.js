2361cd7c8e78f3a41783a2568c75e9fda65339cf
amandaborg93 <amanda.borg13@gmail.com>
2016-03-02 19:40:55 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("hi");
+   alert("BOOM!!!");
+var b, name="Amanda";
+   b=5;
+   b++;
+   console.log(b);
+
+console.log("my name is " + name);
+
+var count=3, counter=1;
+
+if (count>0) {   
+    console.log("Count is greater than zero: " +count);
+} else if (count<0){   
+    console.log("Count is less than " +0);
+} else {
+    console.log("Count is " +0);
+}
+
+while (counter <10){
+    console.log(counter);
+    counter++;   
+}
+    
